422b3a85047cb2a93fd198aab372235a1f4ca7d0
Ajeet Singh <imajeet5@gmail.com>
2022-03-31 17:25:34 +0530

    update Threashold list

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function createObserver() {
   let options = {
     root: null,
     rootMargin: '0px',
-    threshold: 1.0,
+    threshold: buildThresholdList(),
   };
 
   observer = new IntersectionObserver(handleIntersect, options);
